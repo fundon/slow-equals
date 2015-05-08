8fad294cdaefe8ac21f8f5a10e69d37a675c357f
fundon <cfddream@gmail.com>
2015-05-08 16:40:16 +0800

    add ci

---
+++ test/index.test.js
@@ -0,0 +1,35 @@
+var assert = require('assert');
+var slowEquals = require('..');
+
+describe('slow-equals', function() {
+  var a, b, r;
+
+  it('should not equal when lenght not equal', function() {
+    a = 'hello';
+    b = 'hello world';
+    r = slowEquals(a, b);
+
+    assert.equal(false, r);
+
+    r = slowEquals(b, a);
+
+    assert.equal(false, r);
+  });
+
+  it('should equal', function() {
+    a = 'hello';
+    b = 'hello';
+    r = slowEquals(a, b);
+
+    assert.equal(true, r);
+  });
+
+  it('should not equal', function() {
+    a = 'hello';
+    b = 'Hello';
+    r = slowEquals(a, b);
+
+    assert.equal(false, r);
+  });
+
+});
